6dbee7ba071ccddb9f9ff45aa2c113a529cde09a
James Skemp <strivinglife@gmail.com>
2015-03-15 12:15:19 -0500

    Switched over to extend Game.

--- core/src/com/jamesrskemp/spaceassault/SpaceAssaultGame.java
+++ core/src/com/jamesrskemp/spaceassault/SpaceAssaultGame.java
@@ -1,10 +1,11 @@
 package com.jamesrskemp.spaceassault;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 
-public class SpaceAssaultGame extends ApplicationAdapter {
+public class SpaceAssaultGame extends Game {
 
 	@Override
 	public void create () {
@@ -12,7 +13,13 @@ public class SpaceAssaultGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		super.render();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
 	}
 }
