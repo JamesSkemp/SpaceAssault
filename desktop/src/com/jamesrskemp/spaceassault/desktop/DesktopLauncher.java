67b421d3ce944fdb19966cf994949e171166cff8
James Skemp <strivinglife@gmail.com>
2015-03-15 11:55:19 -0500

    Created new libGDX project.

---
+++ desktop/src/com/jamesrskemp/spaceassault/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jamesrskemp.spaceassault.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jamesrskemp.spaceassault.SpaceAssaultGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceAssaultGame(), config);
+	}
+}
---
+++ html/src/com/jamesrskemp/spaceassault/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jamesrskemp.spaceassault.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jamesrskemp.spaceassault.SpaceAssaultGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SpaceAssaultGame();
+        }
+}
