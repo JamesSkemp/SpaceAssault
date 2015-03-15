2e62abe4d327d29cb9c3f3b9a3837eed864ca61c
James Skemp <strivinglife@gmail.com>
2015-03-15 12:06:48 -0500

    Initial configuration updates for size and title.

--- desktop/src/com/jamesrskemp/spaceassault/desktop/DesktopLauncher.java
+++ desktop/src/com/jamesrskemp/spaceassault/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.jamesrskemp.spaceassault.SpaceAssaultGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Space Assault";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new SpaceAssaultGame(), config);
 	}
 }
